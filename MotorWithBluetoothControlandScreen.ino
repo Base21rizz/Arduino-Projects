946c8098a82d0210a69edd18ac7ff295311ecb95
Shoumik Islam <shoumikislamofficial@gmail.com>
2026-02-06 19:25:37 +0600

    Rename MotorWithBluetoothControl to MotorWithBluetoothControlandScreen

--- MotorWithBluetoothControlandScreen.ino
+++ MotorWithBluetoothControlandScreen.ino
@@ -1,9 +1,9 @@
-
+#include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 SoftwareSerial mySerial(10, 11); // RX, TX
 
-int direction{1};
 int outputVar{1};
 int five = 5;
 int six = 6;
@@ -19,30 +19,41 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Engine ON");
+  delay(1000);
+  lcd.clear();
 
   Serial.println("Engine Started!");
 
   // set the data rate for the SoftwareSerial port
   mySerial.begin(9600);
-  mySerial.println("Hello, world?");
+  mySerial.println("Engine Started?");
 }
 
 void loop() { // run over and over
   if (mySerial.available()) {
-    int direction = mySerial.read();
-    direction = direction -'0';
-Serial.write(direction);
+    char direction = mySerial.read();
 
-    if(direction == 1)
+Serial.write(direction);
+lcd.setCursor(0, 0);        // always print on line 2
+lcd.print("Input: ");      // fixed label
+lcd.print(direction);
+lcd.print("   ");          // clears leftover characters
+
+    if(direction == '1')
     {
       digitalWrite(5,LOW);
       digitalWrite(6,HIGH);
     }
-    else if(direction == 2)
+    else if(direction == '2')
     {
       digitalWrite(5,HIGH);
       digitalWrite(6,LOW);
     }
+
     else 
     {
       digitalWrite(5,LOW);
